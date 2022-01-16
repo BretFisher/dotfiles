63a4157b355b0a94206bff9d10c60f5a50175089
Bret Fisher <bret@bretfisher.com>
2022-01-16 12:06:09 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,10 +2,13 @@
 
 # install script for GitHub Codespaces
 # https://docs.github.com/en/codespaces/customizing-your-codespace/personalizing-codespaces-for-your-account#dotfiles
-echo updating apt
 sudo apt-get update
 
-# install silversearcher
-echo installing silversearcher-ag
+# installing apt packages
 sudo apt-get install silversearcher-ag
 
+# installing brew
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
+
+# installing act
+go install github.com/nektos/act@latest
